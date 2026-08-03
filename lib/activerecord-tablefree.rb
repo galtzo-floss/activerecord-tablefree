140182f42e05708a198159584f63c95e0e206a06
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 08:47:08 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/activerecord-tablefree.rb
+++ lib/activerecord-tablefree.rb
@@ -3,10 +3,6 @@ require "activerecord/tablefree"
 require "version_gem"
 require_relative "activerecord/tablefree/version"
 
-ActiveRecord::Tablefree::ActsMethods::Version.class_eval do
-  extend VersionGem::Basic
-end
-
 ActiveRecord::Tablefree::Version.class_eval do
   extend VersionGem::Basic
 end
