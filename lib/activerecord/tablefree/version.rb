efb4d2bda75e46061b1267ae66532413ab351a85
Peter H. Boling <peter.boling@gmail.com>
2026-08-04 01:22:15 -0600

    Restore Tablefree version namespace

--- lib/activerecord/tablefree/version.rb
+++ lib/activerecord/tablefree/version.rb
@@ -2,14 +2,7 @@
 
 module ActiveRecord
   module Tablefree
-    module NoDatabase
-      # Version namespace for this gem.
-      module Version
-        # Current gem version.
-        VERSION = "3.1.8"
-      end
-      # Current gem version exposed at the traditional constant location.
-      VERSION = Version::VERSION # Traditional Constant Location
-    end
+    # Current gem version.
+    VERSION = "3.1.8"
   end
 end
